bccf9ae41054b9d242863d7bbbb89b79fe79137d
sss <sss@karma>
2025-10-20 14:53:39 -0400

    Refresh of gcc15

--- tests/integration/low_level_edm4hep.cxx
+++ tests/integration/low_level_edm4hep.cxx
@@ -1,3 +1,7 @@
+#if __GNUC__ >= 15
+# pragma GCC diagnostic ignored "-Wstringop-overread"
+#endif
+
 // ROOT
 #include <ROOT/RDataFrame.hxx>
 #include <ROOT/RLogger.hxx>
@@ -108,3 +112,4 @@ int main(int argc, const char *argv[]) {
 
   return EXIT_SUCCESS;
 }
+
